f171a4198d7cc610a9f880fb4cb43742b23a2a26
Dylan Stephenson <dylanstep377@gmail.com>
2025-04-04 15:26:58 -0600

    Created base HTML based on wireframes

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  root: "src/",
+
+  build: {
+    outDir: "../dist",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+        header: resolve(__dirname, "src/public/partials/header.html"),
+        footer: resolve(__dirname, "src/public/partials/footer.html"),
+      },
+    },
+  },
+});
